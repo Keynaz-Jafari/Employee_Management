9f8da2f1d913664df7666644c7bde923cd7ad053
Keinazj <jafari.keinaz@gmail.com>
2024-11-29 15:05:42 +0330

    add createUser and edit user

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -8,8 +8,11 @@ import {
 import Login from '../pages/login/login';
 import Dashboard from '../pages/dashboard/dashboard';
 import Users from '../pages/users/users';
+import CreateUser from '../pages/createUser/createUser';
 
 import { UserProvider } from '../context/userContext';
+import EditUser from '../pages/editUser/editUser';
+ 
 
 function App() {
 
@@ -20,11 +23,13 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/users" element={<Users />} />
+          <Route path="/createuser" element={<CreateUser />} />
+          <Route path="/edituser/:id" element={<EditUser />} />
+          
     
 
         </Routes>
       </Router>
-      {/* {token ? null : <Login />} */}
     </UserProvider>
 
   );
